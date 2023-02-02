f84abe4951b9e972a4c6ac383e94b3576ff31020
sanjayk5 <62107994+sanjayk5@users.noreply.github.com>
2023-02-02 16:01:11 +0000

    package assignment done

--- packages/imports/begin/main.go
+++ packages/imports/begin/main.go
@@ -2,11 +2,15 @@
 package main
 
 // import the fmt package from the standard library
+import "fmt"
 
 // import the time package from the standard library
+import "time"
 
 func main() {
 	// use the fmt package to print the string "Hello Gopher!"
+	fmt.Println("Hello Gopher!")
 
 	// use the time package to print the current weekday
+	fmt.Println("Current weekday is ", time.Now().Weekday())
 }
